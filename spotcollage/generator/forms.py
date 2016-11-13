b6854135784222078bde1bf7e5e970d43e333bec
Jorge Hernandez <jm.hernandez@utexas.edu>
2016-11-12 19:13:22 -0600

    generates collage onto a web page

--- spotcollage/generator/forms.py
+++ spotcollage/generator/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class EmailForm(forms.Form):
     your_email = forms.CharField(label='Spotify email:', max_length=30)
-    
-    
+
+class TokenForm(forms.Form):
+    code = forms.CharField(label='', max_length=500)
--- spotcollage/generator/views.py
+++ spotcollage/generator/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-from forms import EmailForm
+from django.shortcuts import render, redirect
+from .collage import auth_url, create_collage
+from .forms import EmailForm, TokenForm
+
+
 def index(request):
     form = None
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
+
             email = form.cleaned_data['your_email']
-            return render(request, 'generator/result.html', {'email':email})
+
+            return redirect(auth_url())
     else:
         form = EmailForm()
     return render(request, 'generator/index.html', {'form': form})
+
+def result(request):
+    if request.method =='GET':
+        form = TokenForm(request.GET)
+        if form.is_valid():
+            token = form.cleaned_data['code']
+            contents = create_collage(token)
+
+        return render(request, 'generator/result.html', {'contents': contents })
+    pass
